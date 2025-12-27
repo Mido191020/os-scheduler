3c73495588588eb4d69ec78b8a54d50a826ce46d
Miod Tarek <at744245@gmail.com>
2025-12-27 21:30:14 +0200

    refactor: decouple simulation engine from scheduling policy
    - Abstract simulation loop into generic  function.
    
    - Implement dependency injection using function pointers for policies.
    
    - Fix state pollution bug by passing workload by value/fresh generation.
    
    - Scale  to 10 jobs to better visualize Convoy Effect.

--- M1/SJF_Scheduler_ALGORITHM.cpp
+++ M1/SJF_Scheduler_ALGORITHM.cpp
@@ -9,12 +9,12 @@ typedef struct {
    int Id;
    int arrival_time=0;
    int duration=0;
-   int remaining;
+   int remaining=0;
    int completion_time;
     status job_status;
 }jobs;
 
-jobs* get_next_job(vector<jobs>&FIFO, int current_time){
+jobs* get_next_job_FIFO(vector<jobs>&FIFO, int current_time){
     for (int i = 0; i < FIFO.size(); ++i) {
         if (FIFO[i].job_status==READY&&FIFO[i].arrival_time<=current_time){
             return &FIFO[i];
@@ -37,128 +37,121 @@ jobs *get_next_job_sjf(vector<jobs>&FIFO,int current_time){
     return selected_job;
 
 }
-void TEST1(){
-    vector<jobs>job;
 
-    for (int i = 3; i > 0; --i) {
+
+
+
+
+
+
+
+vector<jobs> generate_workload(){
+    vector<jobs> workload;
+    int num_jobs = 10;
+
+    for (int i = 0; i < num_jobs; ++i) {
         jobs newJob;
         newJob.Id = i + 1;
         newJob.arrival_time = 0;
-        newJob.duration = (i) * 10;
+        newJob.duration = (num_jobs - i) * 10;
+
         newJob.remaining = newJob.duration;
         newJob.job_status = READY;
-
-        job.push_back(newJob);
+        workload.push_back(newJob);
     }
+    return workload;
+}
+void run_simulation(vector<jobs> workload, jobs* (*policy)(vector<jobs>&, int)) {
 
-    cout<<"\t TEST1 \n";
+    int current_time = 0;
+    int completed_jobs = 0;
 
-    int completed_jobs=0;
-    int current_time=0;
-    while (completed_jobs < 3){
-        jobs *x= get_next_job_sjf(job,current_time);
-        if (x != NULL) {
-            cout << "Time " << current_time << ": Running Job " << x->Id << endl;
+    // Simulation Loop
+    while (completed_jobs < workload.size()) {
+        // Ask the injected policy: "Who should run now?"
+        jobs* current_job = policy(workload, current_time);
 
-            x->remaining--;
+        if (current_job != NULL) {
+            // CPU is busy
+            // Uncomment next line if you want to see tick-by-tick details
+            // cout << "Time " << current_time << ": Running Job " << current_job->Id << endl;
 
-            if (x->remaining == 0) {
-                x->job_status = FINISHED;
-                x->completion_time = current_time + 1;
-                cout << "Job " << x->Id << " Finished!" << endl;
+            current_job->remaining--;
+
+            if (current_job->remaining == 0) {
+                current_job->job_status = FINISHED;
+                current_job->completion_time = current_time + 1;
                 completed_jobs++;
+                cout << "Time " << current_time + 1 << ": Job " << current_job->Id << " Finished!" << endl;
             }
-        }
-
-
-        else{
-            cout<<"Time X IDEL";
+        } else {
+            // CPU is idle
+            cout << "Time " << current_time << ": IDLE" << endl;
         }
         current_time++;
     }
+
+    // Metrics Calculation
     cout << "\n--- Final Metrics ---\n";
-
     double total_turnaround = 0;
-
-    for (int i = 0; i < job.size(); ++i) {
-        int turnaround = job[i].completion_time - job[i].arrival_time;
+    for (int i = 0; i < workload.size(); ++i) {
+        int turnaround = workload[i].completion_time - workload[i].arrival_time;
         total_turnaround += turnaround;
-
-        cout << "Job " << job[i].Id
-             << ": Turnaround Time = " << turnaround
-             << " ticks (Finish: " << job[i].completion_time
-             << " - Arrival: " << job[i].arrival_time << ")\n";
+        cout << "Job " << workload[i].Id << ": Turnaround = " << turnaround << " ticks\n";
     }
 
-    double average_turnaround = total_turnaround / job.size();
+    double avg_turnaround = total_turnaround / workload.size();
     cout << "--------------------------------\n";
-    cout << "Average Turnaround Time = " << average_turnaround << " ticks\n";
-    cout << "--------------------------------\n";
-
-
-}
-void TEST2(){
-    vector<jobs>job;
-   // vector<jobs>job2;
-    for (int i = 0; i <3; ++i) {
-        jobs newJob;
-        newJob.Id = i + 1;
-        newJob.arrival_time = 0;
-        newJob.duration = (i + 1) * 10;
-        newJob.remaining = newJob.duration;
-        newJob.job_status = READY;
-
-        job.push_back(newJob);
-    }
-
-    cout<<"\t TEST2 \n";
-    int completed_jobs=0;
-    int current_time=0;
-    while (completed_jobs < 3){
-        jobs *x= get_next_job_sjf(job,current_time);
-        if (x != NULL) {
-            cout << "Time " << current_time << ": Running Job " << x->Id << endl;
-
-            x->remaining--;
-
-            if (x->remaining == 0) {
-                x->job_status = FINISHED;
-                x->completion_time = current_time + 1;
-                cout << "Job " << x->Id << " Finished!" << endl;
-                completed_jobs++;
-            }
-        }
-
-
-        else{
-            cout<<"Time X IDEL";
-        }
-        current_time++;
-    }
-    cout << "\n--- Final Metrics ---\n";
-
-    double total_turnaround = 0;
-
-    for (int i = 0; i < job.size(); ++i) {
-        int turnaround = job[i].completion_time - job[i].arrival_time;
-        total_turnaround += turnaround;
-
-        cout << "Job " << job[i].Id
-             << ": Turnaround Time = " << turnaround
-             << " ticks (Finish: " << job[i].completion_time
-             << " - Arrival: " << job[i].arrival_time << ")\n";
-    }
-
-    double average_turnaround = total_turnaround / job.size();
-    cout << "--------------------------------\n";
-    cout << "Average Turnaround Time = " << average_turnaround << " ticks\n";
-    cout << "--------------------------------\n";
-
-
+    cout << "Average Turnaround Time = " << avg_turnaround << " ticks\n";
+    cout << "--------------------------------\n\n";
 }
 
+void TEST_FIFO() {
+    cout << "========================================\n";
+    cout << "TEST 1: FIFO Strategy (Suffers Convoy Effect)\n";
+    cout << "========================================\n";
+
+vector<jobs >data=generate_workload();
+    run_simulation(data,get_next_job_FIFO);
+}
+void TEST_SJF() {
+    cout << "========================================\n";
+    cout << "TEST 2: SJF Strategy (Solves Convoy Effect)\n";
+    cout << "========================================\n";
+
+    // 1. Get Fresh Data (Same initial conditions)
+    vector<jobs> data = generate_workload();
+
+    // 2. Run Simulation with SJF Policy
+    run_simulation(data, get_next_job_sjf);
+}
+vector<jobs> generate_late_workload() {
+    vector<jobs> workload;
+
+    // Job 1: Arrives 0, Duration 100 (The Monster)
+    jobs j1; j1.Id=1; j1.arrival_time=0; j1.duration=100; j1.remaining=100; j1.job_status=READY;
+    workload.push_back(j1);
+
+    // Job 2: Arrives 10, Duration 10 (The Mouse)
+    jobs j2; j2.Id=2; j2.arrival_time=10; j2.duration=10; j2.remaining=10; j2.job_status=READY;
+    workload.push_back(j2);
+
+    return workload;
+}
+
+void TEST_LATE_ARRIVAL() {
+    cout << "========================================\n";
+    cout << "TEST 3: Late Arrival (SJF Failure Case)\n";
+    cout << "========================================\n";
+
+    vector<jobs> data = generate_late_workload();
+
+    // We use SJF to prove it fails to preempt
+    run_simulation(data, get_next_job_sjf);
+}
 int main() {
-TEST1();
-TEST2();
+TEST_FIFO();
+TEST_SJF();
+TEST_LATE_ARRIVAL();
 
 }
